4702370d517b2f14d26957ab1e34774db7a563ff
Katherine Bell <kbell@Katherine-MacBookPro.local>
2018-11-17 20:18:25 -0800

    js fixes

--- assets/js/game.js
+++ assets/js/game.js
@@ -105,7 +105,7 @@ $(".timer").html(counter);
 }
 
 function finalScreen() {
-gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+gameHTML = "<p class='text-center'>Radical, thanks for playing!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p class='text-center'>Wrong Answers: " + incorrectTally + "</p>" + "<p class='text-center'>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
 $(".mainArea").html(gameHTML);
 }
 
